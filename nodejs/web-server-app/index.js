105774cd6bcd27657798aa976cb2b0d5fef9cae1
Vishal Kushwaha <133014886+grandeurkoe@users.noreply.github.com>
2025-05-11 20:42:37 +0530

    Add nodejs

---
+++ nodejs/web-server-app/index.js
@@ -0,0 +1,10 @@
+// Import the built-in 'http' module
+const http = require('http');
+
+// Create the server
+const server = http.createServer((req, res) => {
+    res.write("<h1>Hello, World!</h1>");
+    res.end();
+})
+
+server.listen(7800);
